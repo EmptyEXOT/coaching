b486ffd9dfa5f2d9fee952792e29035a92c99cc6
Alim <sadullaev.alim@mail.ru>
2023-12-17 01:48:44 +0300

    Section component for smooth scroll in future

---
+++ src/shared/ui/Section/Section.tsx
@@ -0,0 +1,28 @@
+import React, {FC, ReactNode} from 'react';
+import classNames from "classnames";
+import cls from './Section.module.css'
+import ImagePlaceholder, {ImgPlaceholderBgColor} from "@/shared/ui/Placeholder/ImagePlaceholder/ImagePlaceholder";
+
+export enum BgVariant {
+    Default = 'default',
+    Image = 'image',
+}
+
+interface SectionProps {
+    classNames?: string;
+    children: ReactNode;
+    isFullWidth?: boolean;
+    bg?: BgVariant;
+}
+
+const Section: FC<SectionProps> = (props) => {
+    return (
+        <div className={classNames('mx-auto p-2', props.isFullWidth ? cls.fullWidth : 'container', props.classNames)}>
+            {props.bg === BgVariant.Image ?
+                <ImagePlaceholder bgColor={ImgPlaceholderBgColor.dark} classNames={'absolute -z-10 h-screen'}/> : null}
+            {props.children}
+        </div>
+    );
+};
+
+export default Section;
