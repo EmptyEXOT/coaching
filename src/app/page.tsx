e58896f7373a27a4f751c635d044ba9e763571e2
Alim <sadullaev.alim@mail.ru>
2023-12-19 13:35:07 +0300

    page layout finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-import Navbar from "@/widgets/Navbar/ui/Navbar";
 import Section, {BgVariant} from "@/shared/ui/Section/Section";
 import classNames from "classnames";
-import ImagePlaceholder, {ImgPlaceholderBgColor} from "@/shared/ui/Placeholder/ImagePlaceholder/ImagePlaceholder";
+import ImagePlaceholder from "@/shared/ui/Placeholder/ImagePlaceholder/ImagePlaceholder";
 import Image from "next/image";
-import Icon from './icon.png'
 import Geo from './Geo.png'
 import Mail from './Mail.png'
 import Phone from './Phone.png'
+import Paw from './Paw.png'
 
 export default function Home() {
     return (
         <main className="">
-            <Navbar/>
+            {/*Cover*/}
             <Section bg={BgVariant.Image} isFullWidth={true} classNames={'pt-16 h-screen'}>
-                <div className={classNames('flex justify-center pt-14 z-50 h-full')}>
+                <div className={classNames('flex justify-center pt-14 z-50 h-full px-6')}>
                     <div className={classNames('flex flex-col justify-center')}>
                         <h1 className={classNames('text-5xl/[64px] font-bold text-white text-center max-w-xl mx-auto')}>
                             Unlock Your Full Potential with Coaching
@@ -26,9 +25,11 @@ export default function Home() {
                     </div>
                 </div>
             </Section>
+
+            {/*Feature Section*/}
             <Section classNames={'pt-16 md:pt-32'}>
                 <div className={classNames('flex flex-col lg:flex-row justify-between gap-20')}>
-                    <div className={classNames('flex flex-col justify-between gap-6 flex-1')}>
+                    <div className={classNames('flex flex-col justify-between gap-8 flex-1')}>
                         <p className={classNames('font-semibold')}>Inspire</p>
                         <h1 className={classNames('text-4xl/[48px] font-extrabold')}>Topics That Empower and Transform
                             Lives</h1>
@@ -36,27 +37,27 @@ export default function Home() {
                             reach your full potential. Our coach brings years of experience and expertise to each
                             session, ensuring a transformative and impactful experience for all participants.</p>
                         <div className={classNames('flex flex-col justify-between md:flex-row gap-8 md:pt-6')}>
-                            <div className={classNames('flex flex-col gap-3 max-w-xs')}>
-                                <Image alt={'icon'} src={Icon} width={40}></Image>
+                            <div className={classNames('flex flex-col gap-4 max-w-xs')}>
+                                <Image alt={'icon'} src={Paw} width={40}></Image>
                                 <h4 className={classNames('text-lg font-extrabold')}>Leadership</h4>
                                 <p>Unlocking Your Leadership Potential: Strategies for Success in the Modern
                                     Workplace</p>
                             </div>
-                            <div className={classNames('flex flex-col gap-3 max-w-xs')}>
-                                <Image alt={'icon'} src={Icon} width={40}></Image>
+                            <div className={classNames('flex flex-col gap-4 max-w-xs')}>
+                                <Image alt={'icon'} src={Paw} width={40}></Image>
                                 <h4 className={classNames('text-lg font-extrabold')}>Communication</h4>
                                 <p>Mastering the Art of Effective Communication: Building Stronger Connections</p>
                             </div>
                         </div>
                         <div className={classNames('flex flex-col justify-between md:flex-row gap-8 md:pt-6')}>
-                            <div className={classNames('flex flex-col gap-3 max-w-xs')}>
-                                <Image alt={'icon'} src={Icon} width={40}></Image>
+                            <div className={classNames('flex flex-col gap-4 max-w-xs')}>
+                                <Image alt={'icon'} src={Paw} width={40}></Image>
                                 <h4 className={classNames('text-lg font-extrabold')}>Leadership</h4>
                                 <p>Unlocking Your Leadership Potential: Strategies for Success in the Modern
                                     Workplace</p>
                             </div>
-                            <div className={classNames('flex flex-col gap-3 max-w-xs')}>
-                                <Image alt={'icon'} src={Icon} width={40}></Image>
+                            <div className={classNames('flex flex-col gap-4 max-w-xs')}>
+                                <Image alt={'icon'} src={Paw} width={40}></Image>
                                 <h4 className={classNames('text-lg font-extrabold')}>Communication</h4>
                                 <p>Mastering the Art of Effective Communication: Building Stronger Connections</p>
                             </div>
@@ -72,7 +73,9 @@ export default function Home() {
                     </div>
                 </div>
             </Section>
-            <Section classNames={'pt-16 md:pt-64'}>
+
+            {/*About Section*/}
+            <Section classNames={'pt-32 md:pt-64'}>
                 <div className={classNames('flex flex-col lg:flex-row justify-between gap-20')}>
                     <div className={classNames('hidden lg:flex flex-col justify-center lg:flex-1')}>
                         <div className={classNames('flex justify-center')}>
@@ -81,7 +84,7 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    <div className={classNames('flex flex-col justify-center gap-10 flex-1')}>
+                    <div className={classNames('flex flex-col justify-center gap-8 flex-1')}>
                         <p className={classNames('font-semibold')}>Inspire</p>
                         <h1 className={classNames('text-4xl/[48px] font-extrabold')}>Unlock Your Full Potential with
                             Coaching</h1>
@@ -99,9 +102,11 @@ export default function Home() {
                     </div>
                 </div>
             </Section>
+
+            {/*Contact Section*/}
             <Section classNames={'pt-16 md:pt-64'}>
                 <div className={classNames('flex flex-col lg:flex-row justify-between gap-20')}>
-                    <div className={classNames('flex flex-col justify-center gap-10 flex-1')}>
+                    <div className={classNames('flex flex-col justify-center gap-8 flex-1')}>
                         <p className={classNames('font-semibold text-center')}>Coaching</p>
                         <h1 className={classNames('text-4xl/[48px] font-extrabold text-center')}>Get in Touch</h1>
                         <p className={classNames('text-center')}>Contact us for more information or to schedule a
@@ -114,7 +119,7 @@ export default function Home() {
                             <Image alt={'icon'} src={Mail} width={48}></Image>
                         </div>
                         <h1 className={classNames('text-2xl/[48px] font-extrabold text-center')}>Email</h1>
-                        <p className={classNames('text-center')}>Send us an email and we'll get back to you.</p>
+                        <p className={classNames('text-center')}>Send us an email and we&apos;ll get back to you.</p>
                         <p className={classNames('text-center')}><u>hello@relume.io</u></p>
                     </div>
                     <div className={classNames('flex flex-col justify-between align-middle gap-4')}>
@@ -132,6 +137,136 @@ export default function Home() {
                         <h1 className={classNames('text-2xl/[48px] font-extrabold text-center')}>Office</h1>
                         <p className={classNames('text-center')}>Visit our office for a face-to-face consultation.</p>
                         <p className={classNames('text-center')}><u>123 Sample St, Sydney NSW 2000 AU</u></p>
+                    </div>
+                </div>
+            </Section>
+
+            {/*Team Section*/}
+            <Section classNames={'pt-16 md:pt-64'}>
+                <div className={classNames('flex flex-col justify-center gap-8 flex-1')}>
+                    <p className={classNames('font-semibold')}>Inspiring</p>
+                    <h1 className={classNames('text-4xl/[48px] font-extrabold')}>Meet Our Team</h1>
+                    <p>Learn more about our experienced and qualified coaches.</p>
+                </div>
+                <div className={classNames('flex flex-col justify-between flex-1 pt-16 gap-24')}>
+                    <div className={classNames('flex flex-col justify-between lg:flex-row gap-24 md:pt-6')}>
+                        <div className={classNames('flex flex-col gap-8 w-full sm:flex-row justify-between')}>
+                            <div className={classNames('lg:flex flex-col justify-center sm:flex-1')}>
+                                <div className={classNames('flex justify-center')}>
+                                    <div className={classNames('square')}>
+                                        <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={classNames('flex-1 flex flex-col gap-4')}>
+                                <div>
+                                    <h4 className={classNames('text-xl font-semibold')}>Leadership</h4>
+                                    <h6 className={classNames('text-lg')}>Subheading</h6>
+                                </div>
+                                <p>Unlocking Your Leadership Potential: Strategies for Success in the Modern
+                                    Workplace.Unlocking Your Leadership Potential.</p>
+                            </div>
+                        </div>
+                        <div className={classNames('flex flex-col gap-8 w-full sm:flex-row justify-between')}>
+                            <div className={classNames('lg:flex flex-col justify-center sm:flex-1')}>
+                                <div className={classNames('flex justify-center')}>
+                                    <div className={classNames('square')}>
+                                        <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={classNames('flex-1 flex flex-col gap-4')}>
+                                <h4 className={classNames('text-lg font-extrabold')}>Communication</h4>
+                                <p>Mastering the Art of Effective Communication: Building Stronger Connections</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={classNames('flex flex-col justify-between lg:flex-row gap-24 md:pt-6')}>
+                        <div className={classNames('flex flex-col gap-8 w-full sm:flex-row justify-between')}>
+                            <div className={classNames('lg:flex flex-col justify-center sm:flex-1')}>
+                                <div className={classNames('flex justify-center')}>
+                                    <div className={classNames('square')}>
+                                        <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={classNames('flex-1 flex flex-col gap-4')}>
+                                <h4 className={classNames('text-lg font-extrabold')}>Leadership</h4>
+                                <p>Unlocking Your Leadership Potential: Strategies for Success in the Modern
+                                    Workplace</p>
+                            </div>
+                        </div>
+                        <div className={classNames('flex flex-col gap-8 w-full sm:flex-row justify-between')}>
+                            <div className={classNames('lg:flex flex-col justify-center sm:flex-1')}>
+                                <div className={classNames('flex justify-center')}>
+                                    <div className={classNames('square')}>
+                                        <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={classNames('flex-1 flex flex-col gap-4')}>
+                                <h4 className={classNames('text-lg font-extrabold')}>Communication</h4>
+                                <p>Mastering the Art of Effective Communication: Building Stronger Connections</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Section>
+
+            {/*Gallery Section*/}
+            <Section classNames={'pt-32 md:pt-64'}>
+                <div className={classNames('flex flex-col justify-center gap-8 flex-1')}>
+                    <h1 className={classNames('text-center text-4xl/[48px] font-extrabold')}>Coaching Moments</h1>
+                    <p className={classNames('text-center')}>Capturing the essence of transformative coaching
+                        experiences.</p>
+                </div>
+                <div className={classNames('flex flex-col justify-between gap-6 pt-16')}>
+                    <div className={classNames('flex flex-col justify-between xl:flex-row gap-24 md:pt-6')}>
+                        <div className={classNames('flex flex-col gap-16 w-full lg:flex-row justify-between')}>
+                            <div className={classNames('lg:flex flex-col justify-center sm:flex-1')}>
+                                <div className={classNames('flex justify-center')}>
+                                    <div className={classNames('square')}>
+                                        <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={classNames('lg:max-xl:hidden flex flex-col justify-center sm:flex-1')}>
+                                <div className={classNames('flex justify-center')}>
+                                    <div className={classNames('square')}>
+                                        <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={classNames('lg:max-xl:hidden flex flex-col justify-center sm:flex-1')}>
+                                <div className={classNames('flex justify-center')}>
+                                    <div className={classNames('square')}>
+                                        <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={classNames('hidden lg:max-xl:flex flex-col justify-between gap-8')}>
+                                <div className={classNames('flex flex-col justify-start sm:flex-1 lg:w-72')}>
+                                    <div className={classNames('flex justify-center')}>
+                                        <div className={classNames('square')}>
+                                            <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={classNames('flex flex-col justify-end sm:flex-1 lg:w-72')}>
+                                    <div className={classNames('flex justify-center')}>
+                                        <div className={classNames('square')}>
+                                            <ImagePlaceholder classNames={classNames('h-full absolute')}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Section>
